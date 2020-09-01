6beef62ff833f33724e0e73f857845ae2f452a53
Glenn Dwiyatcita <glenn.dwiyatcita@sap.com>
2020-09-01 11:26:07 +0200

    Committing in accordance with the prophecy.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('*', (req, res) => {
+  res.render('limbo', { text: `Cannot GET ${req.originalUrl}` });
 });
 
 app.listen(port, () => {
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Cah Lanang
+
+After https://youtu.be/nIoknYnDtG8.
+
+## Installation
+
+1. Run `yarn`.
+2. Run `yarn start`.
+3. Visit [http://localhost:8080](http://localhost:8080).
